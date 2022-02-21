7659ec7d0370347b979a59de6fa0d5623ff1c263
arthur-asilva <dev.arthur.silva@gmail.com>
2022-02-21 13:28:56 -0300

    end of template creations

--- core/apps/users/urls.py
+++ core/apps/users/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.LoginView),
+    path('login/', views.LoginView, name='user_login_view'),
+    path('home/', views.HomeView, name='user_home_view'),
+    path('admin/', views.AdminView, name='user_admin_view'),
 ]
